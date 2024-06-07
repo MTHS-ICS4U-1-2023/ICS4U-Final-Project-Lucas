fcb27add1deb5e311d845eb177e33eee83a077b7
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2024-06-07 12:58:55 +0000

    Todo App set up

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,5 @@
-import Hello from "./components/Hello";
 function App() {
-  return (
-    <div className="App">
-      <Hello />
-    </div>
-  );
+  return <div className="App"></div>
 }
 
 export default App;
