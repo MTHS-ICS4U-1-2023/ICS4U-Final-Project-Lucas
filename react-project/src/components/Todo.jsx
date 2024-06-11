b9f9f7407bd3118c1419d1dd79994bfb93491a32
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2024-06-11 14:18:26 +0000

    save inputs

--- react-project/src/components/Todo.jsx
+++ react-project/src/components/Todo.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Todo() {
+    const [todo, setTodo] = useState("");
+    const [todos, setTodosList] = useState([]);
+    function handleSubmit(e) {
+        e.preventDefault();
+        setTodosList([...todos, todo])
+        setTodo("")
+    }
     return (
         <div>
-            <form>
-                <input type="text" />
+            <form on onSubmit={handleSubmit}>
+                <input 
+                    onChange={(e) => setTodo(e.target.value)} 
+                    value={todo}
+                    type="text" 
+                />
                 <button type="submit">Add</button>
             </form>
+            {console.log(todos)}
         </div>
     );
 }
