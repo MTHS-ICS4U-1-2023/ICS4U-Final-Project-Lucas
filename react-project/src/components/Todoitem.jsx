5a0be771b760da4accc1a7f7a684e72970ac06c6
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2024-06-14 23:51:25 +0000

    items crossed out

--- react-project/src/components/Todoitem.jsx
+++ react-project/src/components/Todoitem.jsx
@@ -5,12 +5,17 @@ export default function TodoItem({ item, todos, setTodos }) {
         setTodos(todos.filter((todo)=> todo !== item));
     }
     function handleClick(name) {
-        console.log("Item text clicked", name);
+        setTodos(
+            todos.map((todo)=>
+                todo.name === name ? {...todo, done: !todo.done}: todo 
+            )
+        );
     }
+    const className = item.done ? styles.completed : "";
     return (
         <div className={styles.item}>
             <div className={styles.itemName}> 
-                <span onClick = {() => handleClick(item.name)}> {item.name} </span>
+                <span className = {className} onClick = {() => handleClick(item.name)}> {item.name} </span>
 
                 <span>
                     <button 
